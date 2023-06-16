6d4ba4ff3fe64291dc02478f7e0f75a01d38fc8d
LewisJH27 <lewish36@hotmail.co.uk>
2023-06-16 01:50:24 +0100

    implement the random words, restart and results

--- typing-practice/src/App.tsx
+++ typing-practice/src/App.tsx
@@ -1,11 +1,25 @@
+import { faker } from "@faker-js/faker";
+import RestartButton from "./components/RestartButton";
 
+const words = faker.random.words(10);
 
-function App() {
+const App = () => {
   return (
-    <div className="text-4xl text-center text-blue-500">Typing Practice
-
-    </div>
+    <>
+    <CountdownTimer timeLeft={30} />
+      <GeneratedWords words={words} />
+      <RestartButton
+        className={"mx-auto mt-10 text-slate-500"}
+        onRestart={() => null}
+      />
+    </>
   );
 }
 
+const GeneratedWords = ({ words }: { words: string}) => {
+  return <div className="text-4xl text-center text-slate-500">{words}</div>
+}
+const CountdownTimer = ({timeLeft}: {timeLeft: number}) => {
+  return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>
+}
 export default App;
